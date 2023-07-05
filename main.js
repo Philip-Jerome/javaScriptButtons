3008e1dabf31a9ba17b5a808f511ebb6a1b8e1d1
popotech <99328873+popotech@users.noreply.github.com>
2023-07-05 01:30:43 -0700

    Add files via upload
    JavaScript Buttons

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#red').onclick = function() {
+        document.querySelector('h1').style.color = "red";
+    }
+    document.querySelector('#blue').onclick = function() {
+        document.querySelector('h1').style.color = "blue";
+    }
+    document.querySelector('#green').onclick = function() {
+        document.querySelector('h1').style.color = "green";
+    }
+})
+
+
+
